092b0fafeb68f5ca7872c8437a6e349387e2e0a5
AnthonySedrak <113316979+AnthonySedrak@users.noreply.github.com>
2024-02-21 13:12:15 -0500

    Hi Jeremiah

--- src/Jeremiah_Samuel.java
+++ src/Jeremiah_Samuel.java
@@ -1,5 +1,6 @@
 public class Jeremiah_Samuel {
     public static void main(String[] args) {
         System.out.println("Hi Jeremiah, from Rohan");
+        System.out.println("hey jerry ;)");
     }
 }
